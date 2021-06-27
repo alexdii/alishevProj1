1ac7a12769a4dec93a349a5363e0d8ccd884e342
Alex Dolzhenko <alexdii4000@gmail.com>
2021-06-27 22:51:16 +0300

    This is test on mac.

---
+++ src/alishev/beginner/TestOnMacbook.java
@@ -0,0 +1,16 @@
+package alishev.beginner;
+import java.util.Scanner;
+public class TestOnMacbook {
+
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Input ");
+        String str1 = sc1.nextLine();
+        System.out.println("Result of test: "+str1);
+
+
+
+
+    }
+
+}
