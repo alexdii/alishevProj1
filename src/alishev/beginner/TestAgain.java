df1a194fe1e52c81d1724e16d13fa4091a52a3b9
alexdii <alexdii4000@gmail.com>
2021-06-27 20:55:32 +0300

    Continuing training.

---
+++ src/alishev/beginner/TestAgain.java
@@ -0,0 +1,13 @@
+package alishev.beginner;
+import java.util.Scanner;
+public class TestAgain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your name here: ");
+        String name = sc.nextLine();
+        System.out.println("Your name is "+name+"!");
+
+    }
+
+}
---
+++ src/alishev/beginner/TestOfAge.java
@@ -0,0 +1,11 @@
+package alishev.beginner;
+import java.util.Scanner;
+public class TestOfAge {
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Input your age: ");
+        int ag = ss.nextInt();
+        System.out.println("Your age: "+ag);
+
+    }
+}
