5ca3a6a6727dfb635c3b200001d90d0d8c2f11dd
Admin_2 <alexdii4000@gmail.com>
2021-06-28 12:56:40 +0300

    "If" training

---
+++ src/alishev/beginner/AgainIfTraining.java
@@ -0,0 +1,18 @@
+package alishev.beginner;
+
+public class AgainIfTraining {
+
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 34;
+        if (a < b) {
+            System.out.println("a less than b");
+        } else if (a > b) {
+            System.out.println("a more than b");
+        } else {
+            System.out.println("a equal b");
+        }
+
+
+    }
+}
---
+++ src/alishev/beginner/If.java
@@ -0,0 +1,17 @@
+package alishev.beginner;
+
+public class If {
+
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 4;
+
+        if (a <= b) {
+            System.out.println("a less or equal b");
+        } else {
+            System.out.println("a more than b");
+        }
+
+    }
+
+}
