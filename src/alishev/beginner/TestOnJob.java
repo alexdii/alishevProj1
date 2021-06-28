40287ea2cc593b04e72e97ee45a372f9ae5e273a
Admin_2 <alexdii4000@gmail.com>
2021-06-28 07:56:17 +0300

    new figures

---
+++ src/alishev/beginner/TestOnJob.java
@@ -0,0 +1,16 @@
+package alishev.beginner;
+import java.util.Scanner;
+
+
+public class TestOnJob {
+
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("input something: ");
+        String str = sc1.nextLine();
+        System.out.println("Your result: "+str);
+
+
+    }
+
+}
