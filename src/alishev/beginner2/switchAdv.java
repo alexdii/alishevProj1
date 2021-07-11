4aafb7bb90235491b8ec84ba799fef8452fb2ee4
Alex Dolzhenko <alexdii4000@gmail.com>
2021-07-11 14:38:51 +0300

    beginner2 initializing

---
+++ src/alishev/beginner2/switchAdv.java
@@ -0,0 +1,24 @@
+package alishev.beginner2;
+import java.util.Scanner;
+public class switchAdv {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("введите свой возраст");
+        int age = scr.nextInt();
+        switch(age) {
+            case 0:
+                System.out.println("ты родился");
+                break;
+            case 7:
+                System.out.println("ты пошел в школу");
+                break;
+            case 18:
+                System.out.println("тебе пора идти в армию");
+                break;
+            default:
+                System.out.println("твой возраст не подходит по условиям задачи");
+
+        }
+
+    }
+}
