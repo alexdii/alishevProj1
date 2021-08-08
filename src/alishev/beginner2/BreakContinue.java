45f3ead03a237a213b27be7f90e1300723558563
Alex Dolzhenko <alexdii4000@gmail.com>
2021-08-08 20:36:20 +0300

    Double condition

--- src/alishev/beginner2/BreakContinue.java
+++ src/alishev/beginner2/BreakContinue.java
@@ -4,7 +4,9 @@ public class BreakContinue {
     public static void main(String[] args) {
         for(int i = 0; i<=15; i++) {
             if(i%2==0) {
-                continue;
+                continue; // все, что находится после continue не выполняется 
+                // цикл начинается сначала. условие не выполняется - не выполняется continue.
+                // условие выполняется - выполняется continue и цикл выполняется сначала.
             }
             System.out.println("это нечетное число: "+i);
         }
--- src/alishev/beginner2/switchAdv.java
+++ src/alishev/beginner2/switchAdv.java
@@ -5,17 +5,6 @@ public class switchAdv {
         Scanner scr = new Scanner(System.in);
         System.out.println("введите свой возраст");
         int age = scr.nextInt();
-        if(age>=65) {                            // insert if
-            System.out.println("ты пенсионер");
-        }else if(age>=20) {
-            System.out.println("иди работай!");
-        }else if(age>=18){
-            System.out.println("ты пошел в армию");
-        }else if(age>7){
-            System.out.println("ты учишься в школе");
-        }else if(age>0){
-            System.out.println("ты пошел в детский сад");
-        }
         switch(age) {
             case 0:
                 System.out.println("ты родился");
@@ -23,13 +12,27 @@ public class switchAdv {
             case 7:
                 System.out.println("ты пошел в школу");
                 break;
+            case 18:
+                System.out.println("тебе пора идти в армию");
+                break;
             case 50:
                 System.out.println("ты Лёха");
                 break;
             //default:
             //    System.out.println("твой возраст не подходит по условиям задачи");
-
         }
-
+        if(age>=65) {                            // insert if
+            System.out.println("ты пенсионер");
+        }else if(age>20) {
+            System.out.println("иди работай!");
+        }else if(age>18){
+            System.out.println("ты служишь в армии");
+        }else if(age>7 && age<18){                         //  двойное условие    7<age<18
+            System.out.println("ты учишься в школе");
+        }else if(age>2 && age<7){
+            System.out.println("ты пошел в детский сад");
+        }else if(age>0 && age<=2) {
+            System.out.println("ты маленький ребенок");
+        }
     }
 }
