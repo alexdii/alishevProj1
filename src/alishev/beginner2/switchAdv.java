a0e330729a25a553fb80d3ab88884ce57efb817a
Alex Dolzhenko <alexdii4000@gmail.com>
2021-08-08 11:10:18 +0300

    Change age and conditions

--- src/alishev/beginner2/switchAdv.java
+++ src/alishev/beginner2/switchAdv.java
@@ -5,10 +5,16 @@ public class switchAdv {
         Scanner scr = new Scanner(System.in);
         System.out.println("введите свой возраст");
         int age = scr.nextInt();
-        if(age>65) {                            // insert if
+        if(age>=65) {                            // insert if
             System.out.println("ты пенсионер");
-        }else if(age>20){
+        }else if(age>=20) {
             System.out.println("иди работай!");
+        }else if(age>=18){
+            System.out.println("ты пошел в армию");
+        }else if(age>7){
+            System.out.println("ты учишься в школе");
+        }else if(age>0){
+            System.out.println("ты пошел в детский сад");
         }
         switch(age) {
             case 0:
@@ -17,14 +23,11 @@ public class switchAdv {
             case 7:
                 System.out.println("ты пошел в школу");
                 break;
-            case 18:
-                System.out.println("тебе пора идти в армию");
-                break;
             case 50:
                 System.out.println("ты Лёха");
                 break;
-            default:
-                System.out.println("твой возраст не подходит по условиям задачи");
+            //default:
+            //    System.out.println("твой возраст не подходит по условиям задачи");
 
         }
 
