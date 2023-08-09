f8a7b60c9f7e28edb28ad84e7f125d5c3d9f8c9c
aleks <alexdii4000@gmail.com>
2023-08-09 14:14:26 +0300

    changa language

--- src/alishev/beginner2/switchAdv.java
+++ src/alishev/beginner2/switchAdv.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
 public class switchAdv {
     public static void main(String[] args) {
         Scanner scr = new Scanner(System.in);
-        System.out.println("введите свой возраст");
+        System.out.println("input your age");
         int age = scr.nextInt();
         switch(age) {
             case 0:
-                System.out.println("ты родился");
+                System.out.println("your birthday today");
                 break;
             case 7:
-                System.out.println("ты пошел в школу");
+                System.out.println("you are going to school");
                 break;
             case 18:
-                System.out.println("тебе пора идти в армию");
+                System.out.println("time for army now");
                 break;
-            case 51:
-                System.out.println("ты Лёха");
+            case 52:
+                System.out.println("you are Alex");
                 break;
             //default:
             //    System.out.println("твой возраст не подходит по условиям задачи");
         }
         if(age>=65) {                            // insert if
-            System.out.println("ты пенсионер");
+            System.out.println("pension");
         }else if(age>20) {
-            System.out.println("иди работай!");
+            System.out.println("go job, nigga!");
         }else if(age>18){
-            System.out.println("ты служишь в армии");
+            System.out.println("army serve");
         }else if(age>7 && age<18){                         //  двойное условие    7<age<18
-            System.out.println("ты учишься в школе");
+            System.out.println("school time");
         }else if(age>2 && age<7){
-            System.out.println("ты пошел в детский сад");
+            System.out.println("child-garden");
         }else if(age>0 && age<=2) {
-            System.out.println("ты маленький ребенок");
+            System.out.println("baby");
         }
     }
 }
